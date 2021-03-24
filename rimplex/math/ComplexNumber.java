d5176ed77a344b857eef9262a258ca181660dded
Ava Momberger <momberar@dukes.jmu.edu>
2021-03-24 19:40:43 -0400

    implementation of division

--- rimplex/math/ComplexNumber.java
+++ rimplex/math/ComplexNumber.java
@@ -1,5 +1,11 @@
 package math;
 
+/**
+ * An object class to represent a complex number.
+ * 
+ * @author Ava Momberger && Rhea Morris
+ * @version 3/24/2021
+ */
 public class ComplexNumber
 {
   private double realValue;
@@ -9,7 +15,6 @@ public class ComplexNumber
   {
     this.realValue = realValue;
     this.imaginaryValue = imaginaryValue;
-
   }
 
   public ComplexNumber add(ComplexNumber other)
@@ -37,7 +42,11 @@ public class ComplexNumber
 
   public ComplexNumber divide(ComplexNumber other)
   {
-    return null;
+    double real = ((realValue * other.getRealValue()) + (imaginaryValue * other.getImaginaryValue()))
+        / (Math.pow(imaginaryValue, 2) + Math.pow(other.getImaginaryValue(), 2));
+    double i = ((realValue * other.getRealValue()) - (imaginaryValue * other.getImaginaryValue()))
+        / (Math.pow(imaginaryValue, 2) + Math.pow(other.getImaginaryValue(), 2));
+    return new ComplexNumber(real, i);
   }
 
   public double getRealValue()
@@ -55,15 +64,15 @@ public class ComplexNumber
     String result = "";
     if (realValue == 0 && imaginaryValue != 0)
     {
-      result = "" + imaginaryValue;
+      result = "" + imaginaryValue + "i";
     }
-    else if (realValue != 0 && imaginaryValue == 0)
+    else if (imaginaryValue == 0)
     {
       result = "" + realValue;
     }
     else
     {
-      result = "" + realValue + imaginaryValue;
+      result = "" + realValue + imaginaryValue + "i";
     }
     return result;
   }
