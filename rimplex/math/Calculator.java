f9a5290a624989254c71d4302501d332a9767763
Ava Momberger <momberar@dukes.jmu.edu>
2021-03-24 18:42:16 -0400

    initial template for calculator and complexnumber

---
+++ rimplex/math/Calculator.java
@@ -0,0 +1,10 @@
+package math;
+
+public class Calculator
+{
+  private ComplexNumber recentResult;
+  
+  public Calculator(String input) {
+    
+  }
+}
---
+++ rimplex/math/ComplexNumber.java
@@ -0,0 +1,47 @@
+package math;
+
+public class ComplexNumber
+{
+  private double realValue;
+  private double imaginaryValue;
+  
+  public ComplexNumber(double realValue, double imaginaryValue) 
+  {
+    
+  }
+  
+  public ComplexNumber add(ComplexNumber other)
+  {
+    return null;
+  }
+  
+  public ComplexNumber subtract(ComplexNumber other)
+  {
+    return null;
+  }
+  
+  public ComplexNumber multiply(ComplexNumber other)
+  {
+    return null;
+  }
+  
+  public ComplexNumber divide(ComplexNumber other)
+  {
+    return null;
+  }
+  
+  public double getRealValue()
+  {
+    return realValue;
+  }
+  
+  public double getImaginaryValue()
+  {
+    return imaginaryValue;
+  }
+  
+  public String toString()
+  {
+    return "";
+  }
+}
