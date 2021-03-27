52e4facd7a7b34a2b1eda981a49fd6eaa632f04f
Eric Hernandez-Diaz <herna4er@dukes.jmu.edu>
2021-03-27 12:43:26 -0400

    Created GUIUtils

--- rimplex/app/ButtonHandler.java
+++ rimplex/app/ButtonHandler.java
@@ -6,28 +6,35 @@ import java.awt.event.ActionListener;
 /**
  * ButtonHandler - responds to button presses and inputs.
  * 
- * Modifications:
- * - Anderson (3/25) - added .strip before displaying input string.
+ * Modifications: - Anderson (3/25) - added .strip before displaying input string.
  * 
- * @author Eric Anderson, 
+ * @author Eric Anderson,
  * @version 3/25/2021
  */
 public class ButtonHandler implements ActionListener
 {
 
   @Override
-  public void actionPerformed(ActionEvent e)
+  public void actionPerformed(final ActionEvent e)
   {
     String buttonPressed = e.getActionCommand();
-    String inputString = GUI.getInputFieldText();
-    
+    String inputString = GUIUtils.getInputFieldText();
+
     if (buttonPressed.equals("Reset"))
     {
-      GUI.reset();
+      GUIUtils.reset();
     }
     else if (buttonPressed.equals("Clear"))
     {
-      GUI.clear();
+      GUIUtils.clear();
+    }
+    else if (buttonPressed.equals("="))
+    {
+      String solved = "";
+      // expression = new ArrayList<>();
+      // expression.add(solved);
+      // GUIUtils.updateFields(inputString.strip() + buttonPressed);
+
     }
     else
     {
@@ -35,7 +42,7 @@ public class ButtonHandler implements ActionListener
       {
         GUI.expression.add(inputString.strip());
         GUI.expression.add(buttonPressed);
-        GUI.updateFields("(" + inputString.strip() + ") " + e.getActionCommand());
+        GUIUtils.updateFields("(" + inputString.strip() + ") " + e.getActionCommand());
       }
     }
   }
--- rimplex/app/GUI.java
+++ rimplex/app/GUI.java
@@ -12,101 +12,41 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
- * GUI - Creates objects for displays and input fields. 
+ * GUI - Creates objects for displays and input fields.
  * 
- * Modifications:
- * - Anderson (3/25) - changed input field to JTextField to prevent \n chars.
- *                     Added string formatter to remove \n for redundancy.
+ * Modifications: - Anderson (3/25) - changed input field to JTextField to prevent \n chars. Added
+ * string formatter to remove \n for redundancy.
  * 
- * @author Eric Anderson, 
+ * @author Eric Anderson,
  * @version 3/25/2021
  */
 public class GUI
 {
-  
-  static ArrayList<String> expression = new ArrayList<String>();
-  private static JTextArea display = new JTextArea();
-  private static JTextField inputField = new JTextField();
 
-  public static void main(String[] args) {
+  static ArrayList<String> expression = new ArrayList<String>();
+  static JTextArea display = new JTextArea();
+  static JTextField inputField = new JTextField();
+
+  public static void main(final String[] args)
+  {
     JFrame main;
     JPanel buttonPanel;
-    
+
     main = new JFrame("Rimplex");
     main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     main.setLayout(new GridLayout(3, 1));
     main.setSize(400, 200);
-    
-    buttonPanel = createButtons();
+
+    buttonPanel = GUIUtils.createButtons();
 
     display.setEditable(false);
     display.setBorder(BorderFactory.createTitledBorder("Display")); // temporary border
     inputField.setBorder(BorderFactory.createTitledBorder("Input")); // temporary border
-    
+
     main.add(display);
     main.add(inputField);
     main.add(buttonPanel);
-    
+
     main.setVisible(true);
-  }
-  
-  private static JPanel createButtons() {
-    JButton addButton, subtrButton, multButton, divButton, resetButton, clearButton, equalsButton;
-    
-    JPanel buttonPanel = new JPanel();
-    buttonPanel.setLayout(new FlowLayout());
-    
-    addButton = new JButton("+");
-    divButton = new JButton("\u00F7");
-    multButton = new JButton("\u00D7");
-    subtrButton = new JButton("-");
-    resetButton = new JButton("Reset");
-    clearButton = new JButton("Clear");
-    equalsButton = new JButton("=");
-    
-    ButtonHandler h = new ButtonHandler();
-    resetButton.addActionListener(h);
-    addButton.addActionListener(h);
-    multButton.addActionListener(h);
-    divButton.addActionListener(h);
-    subtrButton.addActionListener(h);
-    clearButton.addActionListener(h);
-    equalsButton.addActionListener(h);
-    
-    buttonPanel.add(clearButton);
-    buttonPanel.add(resetButton);
-    buttonPanel.add(subtrButton);
-    buttonPanel.add(multButton);
-    buttonPanel.add(divButton);
-    buttonPanel.add(addButton);
-    buttonPanel.add(equalsButton);
-    
-    return buttonPanel;
-  }
-  
-  static void reset() {
-    display.setText("");
-    expression = new ArrayList<>();
-    clear();
-  }
-  
-  static void clear() {
-    inputField.setText("");
-  }
-  
-  static void updateFields(String newDisplay) {
-    display.setText(display.getText() + newDisplay + " ");
-    clear();
-  }
-  
-  /**
-   * getInputFieldText - gets user input from field.
-   * 
-   * 
-   * *EMA (3/25) - added logic to remove newlines from expressions. 
-   * @return inputField string.
-   */
-  static String getInputFieldText() {
-    return inputField.getText().replaceAll("\r", "").replaceAll("\n", "");
   }
 }
