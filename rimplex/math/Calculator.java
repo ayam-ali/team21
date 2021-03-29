0ffa6b4b661269482b4f12adb3fa25aac986dd7d
Rhea Morris <morrisrr@dukes.jmu.edu>
2021-03-29 16:29:55 -0400

    NumberFormatException in calculator class

--- rimplex/math/Calculator.java
+++ rimplex/math/Calculator.java
@@ -1,57 +1,96 @@
 package math;
 
+/**
+ *  Computes the user input.
+ * 
+ * @author Ava Momberger && Rhea Morris
+ * @version 3/28/2021
+ */
 import java.util.*;
- 
+
 public class Calculator
 {
   private String recentResult;
-  
-  public Calculator() {
+
+  public Calculator()
+  {
     recentResult = "0";
   }
-  
-  public ComplexNumber calculate(List<String> input) {
+
+  public ComplexNumber calculate(List<String> input)
+  {
     // example input: {"7i", "+", "4-2i"}
     // this part handles running calculations
-    if (isOperation(input.get(0))) {
+    if (isOperation(input.get(0)))
+    {
       input.add(0, recentResult);
     }
-    
+
     ComplexNumber total = parseComplex(input.get(0));
-    for (int i = 2; i < input.size(); i += 2) {
+    for (int i = 2; i < input.size(); i += 2)
+    {
       ComplexNumber num = parseComplex(input.get(i));
-      total = performOperation(total, num, input.get(i-1));
+      total = performOperation(total, num, input.get(i - 1));
     }
-    
+
     recentResult = total.toString();
     return total;
   }
-  
-  private ComplexNumber parseComplex(String numString) {
+
+  private ComplexNumber parseComplex(String numString) throws NumberFormatException
+  {
     // examples: "3+5i", "4", "9i"
     ComplexNumber value = null;
-    if (!numString.contains("+") && !numString.contains("-")) {
+    if (!numString.contains("+") && !numString.contains("-"))
+    {
       // purely imaginary number
-      if (numString.contains("i")) {
+      if (numString.contains("i"))
+      {
         numString = numString.replace('i', Character.MIN_VALUE);
         value = new ComplexNumber(0.0, Double.parseDouble(numString));
-      } else {
+      }
+      else
+      {
         // purely real number
         value = new ComplexNumber(Double.parseDouble(numString), 0.0);
       }
-    } else {
+    }
+    else
+    {
       // number with both real and imaginary parts
       int operation = (numString.contains("+")) ? numString.indexOf("+") : numString.indexOf("-");
-      Double real = Double.parseDouble(numString.substring(0, operation));
-      Double imaginary = Double.parseDouble(numString.substring(operation+1).replace('i', Character.MIN_VALUE));
+      Double real = 0.0;
+      Double imaginary = 0.0;
+
+      // NumberFormatException
+      try
+      {
+        real = Double.parseDouble(numString.substring(0, operation));
+      }
+      catch (NumberFormatException e)
+      {
+        throw new NumberFormatException();
+      }
+
+      try
+      {
+        imaginary = Double
+            .parseDouble(numString.substring(operation + 1).replace('i', Character.MIN_VALUE));
+      }
+      catch (NumberFormatException e)
+      {
+        throw new NumberFormatException();
+      }
       value = new ComplexNumber(real, imaginary);
     }
     return value;
   }
-  
-  private ComplexNumber performOperation(ComplexNumber first, ComplexNumber second, String operand) {
+
+  private ComplexNumber performOperation(ComplexNumber first, ComplexNumber second, String operand)
+  {
     ComplexNumber result = null;
-    switch (operand) {
+    switch (operand)
+    {
       case "+":
         result = first.add(second);
         break;
@@ -69,9 +108,10 @@ public class Calculator
     }
     return result;
   }
-  
-  private boolean isOperation(String string) {
+
+  private boolean isOperation(String string)
+  {
     return string == "+" || string == "-" || string == "*" || string == "/";
   }
-  
+
 }
