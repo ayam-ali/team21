88f40c3ba5b7066d1c864928990f30949bdd227d
Ayam <ali3ah@dukes.jmu.edu>
2021-04-20 18:39:14 -0400

    window adjustment

--- rimplex/app/Driver.java
+++ rimplex/app/Driver.java
@@ -19,7 +19,7 @@ public class Driver
   {
     RimplexWindow gui = new RimplexWindow(new EventHandler());
     gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    gui.setSize(400, 300);
+    gui.setSize(620, 320);
     gui.setVisible(true);
   }
 }
